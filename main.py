bd556259c571896a8af7f2c9693e63893283adda
mchjess <15751@coderacademy.edu.au>
2024-06-05 11:31:13 +1000

    Data types added to Python file.

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-print("Hello world!")
-print(2+4*8)
-print("Don't forget to commit!")
+# print("Hello world!")
+# print(2+4*8)
+# print("Don't forget to commit!")
+
+
+# Comments are ignored
+
+# Data types
+
+# Integers
+#4
+# 7
+# 5
+# If it doesn't have print in front of it then it won't be seen. It won't affect the program either.
+# print(9)
+# print("9")
+# print(type (9))
+# print(type ("9"))
+
+# floats
+# 2.3
+# 5.123123
+# print(3.14515)
+# print (type (-5.63))
+# print (type (-5))
+
+# complex (imaginary numbers) -- not necessary to understand for the course.
+
+2+3j
+print(type(4+7j))
