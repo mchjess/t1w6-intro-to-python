6d4ad844e6d251f69b40acd623387dd1709616db
mchjess <15751@coderacademy.edu.au>
2024-06-05 12:36:16 +1000

    Variables data added.

---
+++ another_main.py
@@ -0,0 +1,18 @@
+# Variables
+
+# snake_case for Python variables
+# CamelCase for JavaScript
+# PascalCase for classes in all the languages
+# kebab-case
+
+num1 = 5
+num2 = 6
+
+print(num1)
+print(num1 + num2)
+
+# If the variable value changes, the latest value will be executed, not the earliest.
+
+num2 = 4
+
+print(num1 - num2)
